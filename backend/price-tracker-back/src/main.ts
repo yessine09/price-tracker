fe3a96fa9d55a55c39944326006d01195d89703b
yessine09 <yessineakaichi09@gmail.com>
2025-02-06 17:46:04 +0100

    Fix Vercel deployment issues

--- backend/price-tracker-back/src/main.ts
+++ backend/price-tracker-back/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: [
+      'http://localhost:5173',
+      'http://localhost:3000',
+      'https://price-tracker-8va2kv5q5-yessine09s-projects.vercel.app',
+    ],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
     credentials: true,
   });
